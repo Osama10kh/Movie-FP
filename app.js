a72337e9d4a4ec2f75877b3ed1df69cb6afd85ce
Osama Khader <osama10kh@gmail.com>
2021-11-21 00:41:24 +0200

    movie time page

---
+++ app.js
@@ -0,0 +1,15 @@
+let home = document.querySelector('.aHome');
+// let search = document.querySelector('.aSearch');
+// let watchlist = document.querySelector('.aWatchlist');
+// let originals = document.querySelector('.aOriginals');
+// let movies = document.querySelector('.aMovies');
+// let series = document.querySelector('.aSeries');
+
+
+function changeColor() {
+    home.style.backgroundColor = 'transparent';
+}
+
+function returnColor() {
+    home.style.backgroundColor = '#f9f9f9';
+}
